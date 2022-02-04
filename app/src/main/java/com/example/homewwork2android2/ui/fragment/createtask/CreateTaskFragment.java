3a5fe1aff6e98c7a0581d4531cd4def14456f8f5
vavKLC <94033595+vavKLC@users.noreply.github.com>
2022-02-04 20:57:25 +0600

    Update CreateTaskFragment.java

--- app/src/main/java/com/example/homewwork2android2/ui/fragment/createtask/CreateTaskFragment.java
+++ app/src/main/java/com/example/homewwork2android2/ui/fragment/createtask/CreateTaskFragment.java
@@ -86,7 +86,7 @@ public class CreateTaskFragment extends BottomSheetDialogFragment implements Dat
         String text = binding.etTask.getText().toString();
         TaskModel taskModel = new  TaskModel(text , date , repaet);
         App.getApp().getDb().taskDao().insert(taskModel);
-        db.collection("Users").document("taskinfo").set(taskModel);
+        db.collection("TaskInfo").document("taskinfo").set(taskModel);
         mDatabase.child("task").child(String.valueOf(taskModel.id)).setValue(taskModel);
     }
 
